c043692ef1eeb63cc188af28115f951f13b12c6a
the-17th-fox <nikitalabuzov@yandex.by>
2023-12-13 00:27:40 +0300

    added db context with save changes overrides

---
+++ backend/EntityFrameworkCore/Helpers/UserIdResolver.cs
@@ -0,0 +1,46 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace EntityFrameworkCore.Helpers;
+
+internal interface IUserIdResolver
+{
+	Guid Resolve();
+}
+
+internal class UserIdResolver : IUserIdResolver
+{
+	public UserIdResolver(IHttpContextAccessor httpContextAccessor)
+	{
+		_httpContextAccessor = httpContextAccessor;
+	}
+	
+	private readonly IHttpContextAccessor _httpContextAccessor;
+
+	public Guid Resolve()
+	{
+		var requestHeaders = _httpContextAccessor.HttpContext.Request.Headers;
+
+		requestHeaders.TryGetValue("Authorization", out var bearerToken);
+
+		var jwtToken = bearerToken.ToString().Remove(startIndex: 0, count: 7); // removing 'Bearer' part
+
+		var userIdClaim = new JwtSecurityTokenHandler()
+			.ReadJwtToken(jwtToken)
+			?.Claims
+			.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+		if (userIdClaim?.Value is null)
+		{
+			throw new ArgumentNullException(nameof(userIdClaim));
+		}
+
+		if (!Guid.TryParse(userIdClaim.Value, out var userId))
+		{
+			throw new ArgumentNullException(nameof(userId));
+        }
+
+		return userId;
+    }
+}
---
+++ backend/EntityFrameworkCore/Infrastructure/MediaStorageDbContext.cs
@@ -0,0 +1,112 @@
+﻿using System.Reflection;
+using EntityFrameworkCore.Entities;
+using EntityFrameworkCore.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EntityFrameworkCore.Infrastructure;
+
+public class MediaStorageDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+	public MediaStorageDbContext(
+		DbContextOptions<MediaStorageDbContext> options,
+		IUserIdResolver userIdResolver)
+		: base(options)
+	{
+		_userIdResolver = userIdResolver ?? throw new ArgumentNullException(nameof(userIdResolver));
+	}
+
+    public DbSet<Media> Media => Set<Media>();
+	public DbSet<Tag> Tags => Set<Tag>();
+	
+	private readonly IUserIdResolver _userIdResolver;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+		base.OnModelCreating(builder);
+	}
+
+	public override int SaveChanges()
+	{
+		UpdateAuditableProperties();
+		return base.SaveChanges();
+	}
+
+	public override int SaveChanges(bool acceptAllChangesOnSuccess)
+	{
+		UpdateAuditableProperties();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+	}
+
+	public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+	{
+		UpdateAuditableProperties();
+        return await base.SaveChangesAsync(cancellationToken);
+	}
+
+	public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+	{
+		UpdateAuditableProperties();
+        return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+	}
+
+	private void UpdateAuditableProperties()
+	{
+		var entries = ChangeTracker.Entries()
+			.Where(e =>
+				(e.State
+					is EntityState.Deleted
+					or EntityState.Modified
+					or EntityState.Added) &&
+				e.Entity is IAuditableEntity);
+
+		foreach (var entry in entries)
+		{
+			switch (entry.State)
+			{
+				case EntityState.Deleted:
+					PerformDelete(entry);
+					break;
+
+				case EntityState.Modified:
+					PerformUpdate(entry);
+					break;
+
+				case EntityState.Added:
+					PerformCreate(entry);
+					break;
+			}
+		}
+	}
+
+	private void PerformDelete(EntityEntry entry)
+	{
+		if (entry.Entity is not IAuditableEntity entity)
+			return;
+
+		entry.State = EntityState.Modified;
+		entity.IsDeleted = true;
+		entity.DeletedBy = _userIdResolver.Resolve();
+		entity.DeletedAt = DateTimeOffset.UtcNow;
+	}
+
+	private void PerformUpdate(EntityEntry entry)
+	{
+		if (entry.Entity is not IAuditableEntity entity)
+			return;
+
+		entity.UpdatedBy = _userIdResolver.Resolve();
+	}
+
+	private void PerformCreate(EntityEntry entry)
+	{
+		if (entry.Entity is not IAuditableEntity entity)
+			return;
+
+		entity.CreatedBy = _userIdResolver.Resolve();
+	}
+}
